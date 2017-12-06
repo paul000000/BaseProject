2a5fc3fdbdbb33ca6cd38305d65e38e4682de63a
paul <paultang630@gmail.com>
2017-12-06 12:21:48 +0800

    test

--- src/test/java/com/github/paul000000/paulTest.java
+++ src/test/java/com/github/paul000000/paulTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class paulTest {
     @Test
     public void getType() throws Exception {
-        paul triangle = new paul(5, 10, 5);
+        paul triangle = new paul(5, 5, 10);
         String result = triangle.getType();
         Assert.assertEquals("不是三角形", result);
    }
